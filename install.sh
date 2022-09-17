c9e39795f150fe10136646a991d00b1d39eb5e84
mhasan49 <mhasan49computation@gmail.com>
2022-09-17 22:48:05 +0200

    install.sh added

--- install.sh
+++ install.sh
@@ -1,54 +1,75 @@
 #!/bin/bash
 {
-	#------------------------------------------------------------------------------------------------
-	# Install jupyterhub on any Debian system
-	#------------------------------------------------------------------------------------------------
-	# REQUIREMENTS
-	# - Currently running Debian Buster or above, ideally minimal, e.g. Raspberry Pi OS Lite-ish =))
-	# - systemd as system/init/service manager
-	# - Either Ethernet connection or local (non-SSH) terminal access
-	#------------------------------------------------------------------------------------------------
-	# Dev notes:
-	# Following items must be exported or assigned to DietPi scripts, if used, until dietpi-obtain_hw_model is executed:
-	# - G_HW_MODEL
-	# - G_HW_ARCH
-	# - G_DISTRO
-	# - G_DISTRO_NAME
-	# - G_RASPBIAN
-	#
-	# The following environment variables can be set to automate this script (adjust example values to your needs):
-	# - GITOWNER='MichaIng'			(optional, defaults to 'MichaIng')
-	# - GITBRANCH='master'			(must be one of 'master', 'beta' or 'dev')
-	# - IMAGE_CREATOR='Mr. Tux'
-	# - PREIMAGE_INFO='Some GNU/Linux'
-	# - HW_MODEL=0				(must match one of the supported IDs below)
-	# - WIFI_REQUIRED=0			[01]
-	# - DISTRO_TARGET=6			[67] (Bullseye: 6, Bookworm: 7)
-	#------------------------------------------------------------------------------------------------
 
-	# Core globals
-	G_PROGRAM_NAME='DietPi-Installer'
+	aAPT_PREREQS=(
 
+		'whiptail' # G_WHIP
+	)
+	for i in "${aAPT_PREREQS[@]}"
+	do
+		dpkg-query -s "$i" &> /dev/null || apt-get -y install "$i" && continue
+		echo -e "[FAILED] Unable to install $i, please try to install it manually:\n\t # apt install $i\n"
+		exit 1
+	done
+	unset -v aAPT_PREREQS
 
-
-
-{
-	
-	if ! [[ $GITBRANCH =~ ^(master|beta|dev)$ ]]
+	if (( $UID ))
 	then
-		aWHIP_BRANCH=(
+		whiptail --title "[FAILED]" --msgbox " Root privileges required, please run this script with sudo " 8 78
+		exit 1
+	fi
 
-			'master' ': Stable release branch (recommended)'
-			'beta' ': Public beta testing branch'
-			'dev' ': Unstable development branch'
-		)
-		if ! GITBRANCH=$(whiptail --title "$G_PROGRAM_NAME" --menu 'Please select the Git branch the installer should use:' --default-item 'master' --ok-button 'Ok' --cancel-button 'Exit' --backtitle "$G_PROGRAM_NAME" 12 80 3 "${aWHIP_BRANCH[@]}" 3>&1 1>&2 2>&3-)
-		then
-			echo -e '[ INFO ] Exit selected. Aborting...\n'
-			exit 0
-		fi
-		unset -v aWHIP_BRANCH
+	whiptail --title "[FAIasdsadLED]" --msgbox " Root privileasdges required, please run this script with sudo " 8 78
+
+	PASSWORD=$(whiptail --passwordbox "please enter your root password" 8 78 --title "password dialog" 3>&1 1>&2 2>&3)
+    # A trick to swap stdout and stderr.
+	# Again, you can pack this inside if, but it seems really long for some 80-col terminal users.
+	exitstatus=$?
+	if [ $exitstatus == 0 ]; then
+    echo "User selected Ok and entered " $PASSWORD
+	else
+    echo "User selected Cancel."
 	fi
 	
-}
+	OPTION=$(whiptail --separate-output --checklist "Choose options" 10 35 5 \
+  	"1" "install_system" ON \
+  	"2" "install_media" OFF \
+  	"3" "install_webbrowser" OFF \
+  	"4" "install_application" OFF \
+	"5" "install_editor" OFF \
+	"6" "install_docker" OFF \
+	"8" "install_settings" OFF 3>&1 1>&2 2>&3)
+	
+	if [ -z "$OPTION" ]; then
+ 	echo "No option was selected (user hit Cancel or unselected all options)"
+	else
+	  for _CHOICE in $OPTION; do
+    case "$_CHOICE" in
+    "1")
+	sed 's/#- install_system/- install_system/g' $PWD/auto_deploy_ubuntu/master.yml -i
+	  ;;
+    "2")
+      sed 's/#- install_media/- install_media/g' $PWD/auto_deploy_ubuntu/master.yml -i
+      ;;
+    "3")
+     sed 's/#- install_webbrowser/- install_webbrowser/g' $PWD/auto_deploy_ubuntu/master.yml -i
+      ;;
+    "4")
+     sed 's/#- install_application/- install_application/g' $PWD/auto_deploy_ubuntu/master.yml -i
+      ;;
+	"5")
+     sed 's/#- install_editor/- install_editor/g' $PWD/auto_deploy_ubuntu/master.yml -i
+      ;;  
+    *)
+      echo "Unsupported item $CHOICE!" >&2
+      exit 1
+      ;;
+    esac
+  	done
+	fi
+   
+   cd $PWD/auto_deploy_ubuntu
+   make install_software
+   
+
 }
